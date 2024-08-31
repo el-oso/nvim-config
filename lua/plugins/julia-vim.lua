75cbe14d43c2a1ee5de51d549f39e323cc0208be
el-oso <ursus.est@gmail.com>
2024-08-31 16:04:56 +0200

    added DAP support for Julia

--- lua/plugins/julia-vim.lua
+++ lua/plugins/julia-vim.lua
@@ -4,4 +4,21 @@ return {
     lazy = false,
   },
   { "kdheepak/JuliaFormatter.vim" },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+      "kdheepak/nvim-dap-julia",
+    },
+    -- Part of the configuration is in the rust-vim.lua file
+    config = function()
+      local dap = require("dap")
+      local ui = require("dapui")
+
+      require("dapui").setup()
+      require("nvim-dap-julia").setup()
+    end,
+  },
 }
